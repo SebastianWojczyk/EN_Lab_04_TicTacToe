76ce8060f2cac0bdb747de6ed6ddc7aa157b8b50
Student <Student@C105_25>
2026-03-17 09:07:44 +0100

    Create buttons.

--- EN_Lab_04_TicTacToe/Form1.cs
+++ EN_Lab_04_TicTacToe/Form1.cs
@@ -12,9 +12,32 @@ namespace EN_Lab_04_TicTacToe
 {
     public partial class Form1 : Form
     {
+        private const int boardSize = 3;
+        private const int fieldSize = 150;
         public Form1()
         {
             InitializeComponent();
+
+            this.AutoSize = true;
+            this.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+
+            PrepareNewGame();
+        }
+
+        private void PrepareNewGame()
+        {
+            for(int column = 0; column< boardSize; column++)
+            {
+                for(int row = 0; row < boardSize; row++)
+                {
+                    Button b = new Button();
+
+                    b.Size = new Size(fieldSize, fieldSize);
+                    b.Location = new Point(column * fieldSize, row * fieldSize);
+
+                    this.Controls.Add(b);
+                }
+            }
         }
     }
 }
